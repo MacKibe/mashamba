adf4e625d9d4b79792c1a9f8910a68ff2312bb4e
Ndichu James <ndichujames2030@gmail.com>
2023-10-10 06:00:20 +0300

    debugged the sql, now its working

--- v/code/tripleM/data/mashamba.sql
+++ v/code/tripleM/data/mashamba.sql
@@ -1,19 +1,18 @@
-#Set the desired type of documents
+# Set the desired type of documents
 set @category='mutation';
 
-#Formulate the ofice query in 3 parts: images, documents and transcription
+# Formulate the office query in 3 parts: images, documents, and transcription
 with
-    #Compile the CTE for driving the images panel
+    # Compile the CTE for driving the images panel
     images as (
         select
             image.document,
-            json_arrayagg(json_object('num', image.page, 'url', image.url, 'name', image.name)) as images,
+            json_arrayagg(json_object('num', image.page, 'url', image.url, 'name', image.name)) as images
         from image
         group by
             document
     ),
-    #
-    #Compiling the CTE for driving the documents panel
+    # Compile the CTE for driving the documents panel
     documents as (
         select
             document.document,
@@ -23,36 +22,34 @@ with
         from document
             inner join image on image.document = document.document
             inner join folder on document.folder = folder.folder
-        
     ),
-    #
-    #Compile teh CTE for dricing the transption panel
-    transcription{
+    
+    # Compile the CTE for driving the transcription panel
+    transcription as (
         select
             document.document,
-            
             json_object(
-                "id" id,
-                "area" area,
-                "person" person,
-                "regno" regno
+                "id", id,
+                "area", area,
+                "person", person,
+                "regno", regno
             ) as transcriptions
         from document
     ),
     #
-    #Compile the CTE for presenting the office
+    # Compile the CTE for presenting the office
     office as (
         select
             document.document,
             images.images,
             documents.documents,
-            transcription.transcription
+            transcription.transcriptions
         from 
             document
             inner join category on document.category = category.category
             left join images on images.document = document.document
             inner join documents on documents.document = document.document
-            inner join transcription on tranmscription.document = document.document
+            inner join transcription on transcription.document = document.document
         where category=@category
     )    
-select * from transcription;
+select * from images;
