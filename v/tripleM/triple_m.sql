bb0c84deba7a2cf0747855c8d4555468779dcad4
Ndichu James <ndichujames2030@gmail.com>
2023-10-27 08:31:15 +0300

    updates my sql for my ts types, added my types in ts

---
+++ v/tripleM/triple_m.sql
@@ -0,0 +1,94 @@
+# Set the desired type of documents
+set @category='mutation';
+
+# Formulate the office query in 3 parts: images, documents, and transcription
+with
+    #List the available categories by document counts
+    list_category as (
+        select 
+            category.category,
+            category.name,
+            count(document.document) as counts
+        from category
+            inner join document on document.category=category.category
+        group by 
+            category.category, 
+            category.name    
+    ), 
+    
+    #The type of this output is:=
+    # type categories = Array<category>
+    # type category = {pk:number, name:string, documents:number}
+    report_category as (
+        select
+            json_arrayagg(
+                json_object(
+                    "pk", category,
+                    "name", name,
+                    "documents", counts
+                )
+            ) as category
+        from 
+            list_category
+    ),
+
+    # Compile the CTE for driving the images panel
+    images as (
+        select
+            image.document,
+            json_arrayagg(json_object('num', image.page, 'url', image.url, 'name', image.name)) as images
+        from image
+        group by
+            document
+    ),
+    # Compile the CTE for driving the documents panel
+    documents as (
+        select
+            document.document,
+            #
+            # A visible link for driving the documents panel
+            concat_ws("/", document.id, person, area) as documents
+        from document
+            inner join image on image.document = document.document
+            inner join folder on document.folder = folder.folder
+    ),
+    
+    # Compile the CTE for driving the transcription panel
+    transcription as (
+        select
+            document.document,
+            json_object(
+                "id", id,
+                "area", area,
+                "person", person,
+                "regno", regno
+            ) as transcriptions
+        from document
+    ),
+    #
+    # Compile the CTE for presenting the office
+    document as (
+        select
+                json_object(
+                    "pk", document.document,
+                    "image", images.images,
+                    "name", documents.documents,
+                    "transcrption", transcription.transcriptions
+                ) x
+        from 
+            document
+            inner join category on document.category = category.category
+            left join images on images.document = document.document
+            inner join documents on documents.document = document.document
+            inner join transcription on transcription.document = document.document
+        where category.name=@category
+        limit 1
+    ),
+    
+    office as (
+        select
+            json_arrayagg(x) as office
+        from
+            document
+    )
+select * from report_category;
--- v/tripleM/triple_m.ts
+++ v/tripleM/triple_m.ts
@@ -1,12 +1,9 @@
 // Access the server services by importing all the facilities that are in the
 //server module in the schema foler of our library
-import * as server from "../../../../../schema/v/code/server.js";
+import * as server from "../../../schema/v/code/server.js";
 //
 // Access to Page class of our library
-import * as view from "../../../../../outlook/v/code/view.js";
-// 
-// Documents category
-type category = 'title_deed'|'mutation'|'cert_of_title'|'others';
+import * as view from "../../../outlook/v/code/view.js";
 //
 // Get the documents to drive our page.
 // A document has the following structure:-
@@ -20,20 +17,29 @@ type criteria =
   name: category
 };
 // 
-// Fields
-type fields = "document" | "title_no" | "category" | "area" | "owner" | "regno";
-// 
-// Images
-type images = { 
-  url: string;  
-};
-// 
-// Document
-type document = {
-    name:string,
-    images:Array<image>,
-    fields:Array<fields>
+// didnt include the pk
+type category = {
+  name:string;
+  no_of_documents:number;
 }
+// 
+// 
+type transcription = "id"|"area"|"regno"|"person";
+// 
+//
+type image = {
+  num:number;
+  url:string;
+}
+// 
+//
+type documents = {
+    pk:number;
+    name:string;
+    image:Array<images>;
+    transcription:transcription;
+}
+
 //
 // Extend the page class with our own version, called mashamba
 export class triple_m extends view.page {
@@ -69,49 +75,51 @@ export class triple_m extends view.page {
   // Lists all the documents from the database in the document panel
   public async load_documents(criteria?:criteria): Promise<void> {
     //
-    const sql = this.get_criteta_sql(criteria);
+    const sql = this.get_criteria_sql(criteria);
     // 
     //  Excecute the sql
-    const documents: Array<{name:string, images:string, fields:string}> = await server.exec(
+    const x: Array<string> = await server.exec(
       'database',
       ['mutall_mashamba'],
       'get_sql_data',
       [sql]
     );
+    const y:documents = JSON.parse(x[0])
+    //
     // 
-    // Append the list of documents to each li element
-    documents.forEach(doc=>this.load_document(doc))
   };
   // 
-  get_criteta_sql(criteria) {
+  get_criteria_sql(criteria):string {
     //
-    // Get the documents using a query
-    const sql = `
-      select
-        document.document,
-        #
-        # A visible link for driving the documents panel
-        concat_ws("/", document.id, person, area) as documents
-      from document
-        inner join image on image.document = document.document
-        inner join folder on document.folder = folder.folder
-    `;
+    //Start with an empty sql
+    let sql:string;
+    //
+    //Get the ctes from '/mashamba/v/triple_m/triple_m.sql'
+    //
+    //Complete the ctes with the clause for retrieving office,
+    //select * from office
+    //
+    //Replace @category with the criteria
+    //
+    //Retturn the complete sqs
+    
   }
-  load_document(document:{name:string, images:string, fields:string}):void{
+  load_document(document:documents):void{
     // 
     // Create a list element
-    const item = this.show_documents_panel(document.name);
+    const item:string = this.show_documents_panel(document.name);
     //
-    const images:Array<image> = JSON.parse(document.images);
-    const fields:Array<field> = JSON.parse(document.fields)
+    const images:Array<images> = JSON.parse(document.image);
+    const fields:Array<transcription> = JSON.parse(document.transcription);
     //
     // 1. Add event listener to the button when its clicked it shows images of the specific document 
     // and its transcription.
     item.onclick = () => {
         images.forEach(Image=>this.display_image(Image));
         fields.forEach(Field=>this.display_field(Field))
+    }
   }
-  // 
+    // 
   // Loads documents on document panel
   show_documents_panel(document.name){
     //
@@ -126,27 +134,9 @@ export class triple_m extends view.page {
   //
   // Display images of the documents when a specific document is selected.
   display_image(images:image) {
-    //
-    // Get the images
-    //
-    // Get reference to the images panel
-    //
-    // Append the images to the images panel
   }
   //
   // Fill in the transcriptions when document is selected
-  display_field(fields:field) {
-    //
-    //Skip the pages key (because it is a special key)
-    if (key === "pages") return;
-    //
-    //Get the named element
-    const element = <HTMLInputElement>document.getElementById(field);
-    //
-    //Get the value that maches the key
-    const value = this.docs![this.counter][field];
-    //
-    //Set the element vale only if the value is not null
-    if (value !== null) element.value = String(value);
+  display_field(transciptions:transcription) {
   }
 }
