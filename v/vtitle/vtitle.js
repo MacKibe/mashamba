0d0fb3f3da4d414cfb8a9644d6d243edd81de951
Ndichu James <ndichujames2030@gmail.com>
2024-01-25 08:10:29 +0300

    done with diaplaying my vtitles and the number of docs in each category

--- v/vtitle/vtitle.js
+++ v/vtitle/vtitle.js
@@ -32,19 +32,59 @@ class mashamba extends view {
         // Get the number of elements in the table head to populate the td for the new rows.
         const number_of_tds = thead.getElementsByTagName("th").length;
         //
-        // Create a new tr to appen to the tbody
-        const new_row = document.createElement("tr");
+        // Display the vtitle in the new rows
+        data.forEach((vtitle) => {
+            //
+            // Create a new tr to appen to the tbody
+            const new_row = document.createElement("tr");
+            //
+            // Create td to append the data
+            const new_cell = document.createElement("td");
+            //
+            // Create td to append mutatino data
+            const new_mutation_cell = document.createElement("td");
+            //
+            // Create td to append other data
+            const new_others_cell = document.createElement("td");
+            //
+            // Create td to append titleDeed data
+            const new_title_deed_cell = document.createElement("td");
+            //
+            // Get the mutaions docs
+            const mutation_docs = Object.keys(vtitle.docs)
+                .filter((category) => category === "mutation")
+                .map((category) => vtitle.docs[category]);
+            //
+            // Get the mutaions docs
+            const title_deed_docs = Object.keys(vtitle.docs)
+                .filter((category) => category === "title")
+                .map((category) => vtitle.docs[category]);
+            //
+            // Get the mutaions docs
+            const other_docs = Object.keys(vtitle.docs)
+                .filter((category) => category === "other")
+                .map((category) => vtitle.docs[category]);
+            //
+            // Append data to the cells
+            new_cell.textContent = `${vtitle.id}`;
+            new_mutation_cell.textContent = `${title_deed_docs.length}`;
+            new_others_cell.textContent = `${other_docs.length}`;
+            new_title_deed_cell.textContent = `${mutation_docs.length}`;
+            //
+            // Append the cells to the tr
+            new_row.appendChild(new_cell);
+            new_row.appendChild(new_title_deed_cell);
+            new_row.appendChild(new_mutation_cell);
+            new_row.appendChild(new_others_cell);
+            //
+            // Append the tr to the tbody
+            tbody.appendChild(new_row);
+            //
+            // For testing
+            console.log(vtitle.docs);
+        });
         //
-        // Create td to append the data
-        const new_cell = document.createElement("td");
-        //
-        //
-        console.log(number_of_tds);
-        //
-        // Loop through each vtitle and append id to the td.
-        // data.forEach((vtitle) => {
-        //   console.log(vtitle);
-        // });
+        // Add cells to tht tr.
     }
 }
 new mashamba().display(vtitles);
--- v/vtitle/vtitle.ts
+++ v/vtitle/vtitle.ts
@@ -86,23 +86,57 @@ class mashamba extends view {
     // Get the number of elements in the table head to populate the td for the new rows.
     const number_of_tds = thead.getElementsByTagName("th").length;
     //
-    // Create a new tr to appen to the tbody
-    const new_row: HTMLElement = document.createElement("tr");
-    //
-    // Create td to append the data
-    const new_cell: HTMLElement = document.createElement("td");
-    // 
-    // Append data to the cells
-    // Destructure the vtitles 
-    const {id,docs} = data
-    // 
-    // Append the cells to the tr
-    //
-    // Append the tr to the tbody
-    tbody.appendChild(new_row);
-    // 
-    //  
-    
+    // Display the vtitle in the new rows
+    data.forEach((vtitle: vtitle) => {
+      //
+      // Create a new tr to appen to the tbody
+      const new_row: HTMLElement = document.createElement("tr");
+      //
+      // Create td to append the data
+      const new_cell: HTMLElement = document.createElement("td");
+      //
+      // Create td to append mutatino data
+      const new_mutation_cell: HTMLElement = document.createElement("td");
+      //
+      // Create td to append other data
+      const new_others_cell: HTMLElement = document.createElement("td");
+      //
+      // Create td to append titleDeed data
+      const new_title_deed_cell: HTMLElement = document.createElement("td");
+      //
+      // Get the mutaions docs
+      const mutation_docs = Object.keys(vtitle.docs)
+        .filter((category) => category === "mutation")
+        .map((category) => vtitle.docs[category]);
+      //
+      // Get the mutaions docs
+      const title_deed_docs = Object.keys(vtitle.docs)
+        .filter((category) => category === "title")
+        .map((category) => vtitle.docs[category]);
+      //
+      // Get the mutaions docs
+      const other_docs = Object.keys(vtitle.docs)
+        .filter((category) => category === "other")
+        .map((category) => vtitle.docs[category]);
+      //
+      // Append data to the cells
+      new_cell.textContent = `${vtitle.id}`;
+      new_mutation_cell.textContent = `${title_deed_docs.length}`;
+      new_others_cell.textContent = `${other_docs.length}`;
+      new_title_deed_cell.textContent = `${mutation_docs.length}`;
+      //
+      // Append the cells to the tr
+      new_row.appendChild(new_cell);
+      new_row.appendChild(new_title_deed_cell);
+      new_row.appendChild(new_mutation_cell);
+      new_row.appendChild(new_others_cell);
+      //
+      // Append the tr to the tbody
+      tbody.appendChild(new_row);
+      //
+      // For testing
+      console.log(vtitle.docs);
+    });
   }
 }
 
